ed87d946fc5a2c0a59dfcd082a14661875d587a3
Rashad <rashadabayram@gmail.com>
2021-03-29 17:04:05 -0600

    head function added

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+function head(arr){
+  return arr.shift(1);
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
