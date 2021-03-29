7aa65139eb67a7b5818c15ba702aefaed9837ba8
Rashad <rashadabayram@gmail.com>
2021-03-29 17:34:35 -0600

    ran eslint

--- tail.js
+++ tail.js
@@ -7,10 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(tail) {
-  return tail.slice(1)
-}
+  return tail.slice(1);
+};
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
