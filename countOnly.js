4f341e2622dab06dbc48957db97bb1df905e5f12
Rashad <rashadabayram@gmail.com>
2021-03-31 14:37:37 -0600

    results object added

--- countOnly.js
+++ countOnly.js
@@ -12,3 +12,32 @@ assertEqual('LightHouse Labs', 'Bootcamp');
 assertEqual(1, 1);
 assertEqual(1, "adas");
 
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  
+  const results = {}
+  return results;
+
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
