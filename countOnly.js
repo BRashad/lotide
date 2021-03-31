8e34f703c4c7d441de89b26c9df483dca5537aca
Rashad <rashadabayram@gmail.com>
2021-03-31 14:40:28 -0600

    added initial for loop

--- countOnly.js
+++ countOnly.js
@@ -7,16 +7,21 @@ const assertEqual = function(actual, expected) {
 };
 
 
-//TEST CODE
-assertEqual('LightHouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(1, "adas");
+// //TEST CODE
+// assertEqual('LightHouse Labs', 'Bootcamp');
+// assertEqual(1, 1);
+// assertEqual(1, "adas");
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   
   const results = {}
+
+  for (const item of allItems) {
+    console.log(item);
+  }
+
   return results;
 
 }
@@ -38,6 +43,6 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+//assertEqual(result1["Agouhanna"], undefined);
 
 
