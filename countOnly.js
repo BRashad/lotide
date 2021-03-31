b46f2460dfc82f43fd93dcdda42bc13e9ebd6ded
Rashad <rashadabayram@gmail.com>
2021-03-31 14:43:12 -0600

    added final if statemnt to count only what we need

--- countOnly.js
+++ countOnly.js
@@ -19,6 +19,15 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {}
 
   for (const item of allItems) {
+
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+
     console.log(item);
   }
 
