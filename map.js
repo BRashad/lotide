64268152748cf1127259b4e7353477a537c5642b
Rashad <rashadabayram@gmail.com>
2021-04-01 16:53:26 -0600

    map final code

---
+++ map.js
@@ -0,0 +1,26 @@
+const eqArrays = (arr1, arr2) => {
+  return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
+};
+
+const assertArraysEqual = (array1, array2) => {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`Assertion Passed: True`);
+  } else {
+    console.log(`Assertion Failed: False`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
