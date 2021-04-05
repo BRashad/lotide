52327ab9c126303396804288eaefdd5b86b11aa5
Rashad Bayram <rashadabayram.com>
2021-04-05 13:57:18 -0600

    assertArrayEqual test files with links added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const eqArrays = (arr1, arr2) => {
-  return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
-};
-
-// assertEqual(eqArrays([4, 3, 3], [4, 3, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [1, 5, 3]), false); // => should PASS
 
 const assertArraysEqual = (array1, array2) => {
   if (eqArrays(array1, array2) === true) {
@@ -21,4 +9,4 @@ const assertArraysEqual = (array1, array2) => {
   }
 };
 
-assertArraysEqual([1, 3, 3], [1, 3, 3]);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqual.js
@@ -0,0 +1,3 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 3, 3], [1, 3, 3]);
